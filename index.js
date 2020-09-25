46f2a3fe1d2965509d2f730f8dcc7938c9411c47
LargatSeifeddine <largatseif@outlook.com>
2020-09-25 12:01:05 +0000

    new xl

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
-const convCsvToXlsx = require('@aternus/csv-to-xlsx');
-const fs = require('fs');
 
-const path = require('path');
-const convertCsvToXlsx = require('@aternus/csv-to-xlsx');
+const xl = require('excel4node');
+const csvFilePath = './data.csv'
+const csv = require('csvtojson');
+const headers_E = [
+		'STATUT',
+		'FOURNISSEUR',
+		'FACTURE F',
+		'FACTURE ENGIE',
+		'HT',
+		'TTC',
+		'DVS',
+		'EMISSION',
+		'ECHEANCE',
+		'COMMANDE',
+		'ENTITE',
+		'AFFAIRE',
+		'RA']
+csv({
+	delimiter:';',
+	headers:headers_E
+})
+	.fromFile(csvFilePath)
+	.then((jsonObj) => {
 
-let source = path.join(__dirname, 'data.csv');
-let destination = path.join(__dirname, 'converted_report.xlsx');
+		// console.log(jsonObj);
+		const wb = new xl.Workbook();
+    	const ws = wb.addWorksheet('devENGIE');
+		const headingColumnNames = headers_E;
 
-try {
-  convertCsvToXlsx(source, destination);
-} catch (e) {
-  console.error(e.toString());
-}
+		let rowIndex = 2;
+		let headingColumnIndex = 1;
+		headingColumnNames.forEach(heading => {
+			ws.cell(1, headingColumnIndex++)
+				.string(heading)
+		});
+		jsonObj.forEach( record => {
+			let columnIndex = 1;
+			Object.keys(record ).forEach(columnName =>{
+				ws.cell(rowIndex,columnIndex++)
+					.string(record [columnName])
+			});
+			rowIndex++;
+    	});
+		wb.write('filename.xlsx');
+	});
