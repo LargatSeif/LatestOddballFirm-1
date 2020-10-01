36e7e62316564fb0025757dc0a1571d345206d75
LargatSeifeddine <largatseif@outlook.com>
2020-10-01 16:31:51 +0000

    xxxxxxzzzzz

--- index.js
+++ index.js
@@ -1,38 +1,51 @@
 
-const xl = require('excel4node');
+const iconv = require('iconv-lite');
 const fs = require('fs');
 const csvFilePath = './data1.csv'
 const csv = require('csvtojson');
+const utf8 = require('utf8');
+let Blob = require ('cross-blob')
+let ch = 'Bloquée'
 
+console.log("######", Buffer.from(iconv.encode(ch,'utf8'))) ;
+ 
+const data_brut = `STATUT;FOURNISSEUR;FACTURE F;FACTURE ENGIE;HT;TTC;DVS;EMISSION;ECHEANCE;COMMANDE;ENTITE;AFFAIRE;RA;
+En cours de traitement;TOTAL MARKETING FRANCE;F0900005;9000662075;17278,94;20734,72;EUR;31/01/2020;10/07/2020;;INEO;SSA140337; 
+En litige;TOTAL MARKETING FRANCE;F0900005;9000662075;17278,94;20734,72;EUR;31/01/2020;10/07/2020;;INEO;SSA140337;
+Bloquée;EG RETAIL (FRANCE);98556132;9000000002;295,12;354,15;EUR;03/02/2020;03/02/2020;Zheyun GU;INEO;SSA124838;Zhéyun GU;
+En cours de traitement;ACCES INDUSTRIE;F19N2049;5120102747;411,9;494,28;EUR;29-11-2019;28-01-2020;;INEO LOGISTIQUE;SLG3C2322;`;
 
-console.log()
-const headers_E = [
-		'STATUT',
-		'FOURNISSEUR',
-		'FACTURE F',
-		'FACTURE ENGIE',
-		'HT',
-		'TTC',
-		'DVS',
-		'EMISSION',
-		'ECHEANCE',
-		'COMMANDE',
-		'ENTITE',
-		'AFFAIRE',
-		'RA']
 function transformToXls(content){
 	return new Promise ((resolve,reject) => {
-		content = content.replace(/Bloqu√©e/g,'Bloquée');
 		content = content.replace(/;/g,'\t');
+		// console.log(content)
 		resolve(content);
 	});
 }
+// 
 
-let data = fs.readFileSync(csvFilePath,{encoding:'utf8'});
-transformToXls(data).then((data)=>{
 
-fs.writeFileSync('abc.xls',data)
+
+/*
+let decodedFile = iconv.decode(data_brut, 'iso88591');
+
+if(decodedFile.indexOf('é') < 0) { 
+	console.log('mac')
+	decodedFile = iconv.decode(data_brut, 'macintosh'); 
+}
+
+*/
+var blob = new Blob(["\ufeff"+data_brut], {
+    type: 'text/csv; charset=utf-8'
+});
+transformToXls("\ufeff"+data_brut).then((data)=>{
+	
+	fs.writeFileSync('le_bon_encodage.xls',data,'utf8');
+	console.log(data)
 })
+ 
+/*
+*/
 		/*
 csv({
 	delimiter:';',
